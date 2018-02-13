f318d039c1b1a06cf6565cb8b295a43d92953ba8
KerychCool <32357145+KerychCool@users.noreply.github.com>
2018-02-14 00:33:05 +0200

    Add files via upload

---
+++ lab2_1.java
@@ -0,0 +1,12 @@
+
+public class lab2_1 {
+	static int i;
+	static char c;
+	public static void main(String[] args){
+		System.out.println("--");
+		System.out.println(i);
+		System.out.println("--");
+		System.out.println(c);
+		System.out.println("--");
+}
+}
