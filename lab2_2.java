ee109c3641a9efa6bf5c357c0da7af9362b6a65f
KerychCool <32357145+KerychCool@users.noreply.github.com>
2018-02-14 00:25:51 +0200

    Add files via upload

---
+++ lab2_2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class lab3-1 {
+	public static void main(String[] args){
+		System.out.println("Hello world.");
+		System.out.println("Today is: ");
+		System.out.println(new Date());
+	}
+}
