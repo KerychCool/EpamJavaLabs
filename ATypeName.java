6f2f36f2b9f128c91e098905f80440bc010184ee
KerychCool <32357145+KerychCool@users.noreply.github.com>
2018-02-14 02:06:50 +0200

    Add files via upload

---
+++ ATypeName.java
@@ -0,0 +1,6 @@
+
+public class ATypeName {
+	public int i = 3;
+	public int t = 6;
+	public int result;
+}
---
+++ lab2_3.java
@@ -0,0 +1,7 @@
+public class lab2_3 {
+	public static void main(String[] args) {
+		ATypeName Multiplicate = new ATypeName();
+		Multiplicate.result = Multiplicate.i*Multiplicate.t;
+		System.out.println(Multiplicate.result);
+	}
+}
