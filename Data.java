192db0410f1c651c71db67133f18e61e129758b7
KerychCool <32357145+KerychCool@users.noreply.github.com>
2018-03-06 13:29:29 +0200

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,7 @@
+public class Data {
+	
+    public int i = 25;
+    public double d  = 378.75d;
+    public boolean b = true;
+
+}
---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[]args){
+      Data variable = new Data();
+      System.out.println(variable.i);
+      System.out.println(variable.d);
+      System.out.println(variable.b);
+  }
+}
